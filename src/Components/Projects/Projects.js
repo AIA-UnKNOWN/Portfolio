e55536f042da112cd8f359360e849b74ce389471
Ajboy Ian Abordo <aabordo@beyondultratechnologies.com>
2021-08-24 21:32:30 +0800

    deployment fixed

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,6 +1,5 @@
 import './Projects.css';
-import React, { useState } from 'react';
-import { useEffect } from 'react/cjs/react.development';
+import React, { useState, useEffect } from 'react';
 
 function Projects() {
 	const [ projects, setProjects ] = useState([
