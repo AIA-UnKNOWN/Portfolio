672d8064f75bcef3c7e70daaa49a4d4c6ccc8e8f
Ajboy Ian Abordo <aabordo@beyondultratechnologies.com>
2021-08-24 21:57:46 +0800

    Added parallax in the background

--- src/App.js
+++ src/App.js
@@ -5,9 +5,20 @@ import Projects from './Components/Projects/Projects';
 import Contact from './Components/Contact/Contact';
 import Footer from './Components/Footer/Footer';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    const app = document.querySelector('.App');
+
+    window.addEventListener( 'scroll' , function() {
+      const scrollY = window.scrollY;
+
+      app.style.backgroundPositionY = `${-scrollY * 0.1}px`;
+    });
+  });
+
   return (
     <div className="App">
       <Header />
