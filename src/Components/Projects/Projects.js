2f5b6e4cc185cdc804339c0dc4d10ccce15e44d2
Ajboy Ian Abordo <aabordo@beyondultratechnologies.com>
2021-08-28 14:15:57 +0800

    My portfolio is now responsive in mobile devices

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,27 +1,10 @@
 import './Projects.css';
 import React, { useState, useEffect } from 'react';
 
+import { dummyProjects } from './dummy-projects';
+
 function Projects() {
-	const [ projects, setProjects ] = useState([
-		{
-			name: 'Project 1',
-			stacks: [ 'HTML', 'CSS', 'Javascript', 'ReactJS', 'MySQL' ],
-			image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSN26hOftzocJIhBD4xrhROdm8fcchqALyBwQ&usqp=CAU',
-			url: '#'
-		},
-		{
-			name: 'Project 2',
-			stacks: [ 'HTML', 'CSS', 'Javascript', 'ReactJS', 'MySQL' ],
-			image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSN26hOftzocJIhBD4xrhROdm8fcchqALyBwQ&usqp=CAU',
-			url: '#'
-		},
-		{
-			name: 'Project 3',
-			stacks: [ 'HTML', 'CSS', 'Javascript', 'ReactJS', 'MySQL' ],
-			image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSN26hOftzocJIhBD4xrhROdm8fcchqALyBwQ&usqp=CAU',
-			url: '#'
-		}
-	]);
+	const [ projects, setProjects ] = useState(dummyProjects);
 
 	useEffect(() => {
 		const percent = 80;
---
+++ src/Components/Projects/dummy-projects.js
@@ -0,0 +1,20 @@
+export const dummyProjects = [
+	{
+		name: 'Project 1',
+		stacks: [ 'HTML', 'CSS', 'Javascript', 'ReactJS', 'MySQL' ],
+		image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSN26hOftzocJIhBD4xrhROdm8fcchqALyBwQ&usqp=CAU',
+		url: '#'
+	},
+	{
+		name: 'Project 2',
+		stacks: [ 'HTML', 'CSS', 'Javascript', 'ReactJS', 'MySQL' ],
+		image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSN26hOftzocJIhBD4xrhROdm8fcchqALyBwQ&usqp=CAU',
+		url: '#'
+	},
+	{
+		name: 'Project 3',
+		stacks: [ 'HTML', 'CSS', 'Javascript', 'ReactJS', 'MySQL' ],
+		image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSN26hOftzocJIhBD4xrhROdm8fcchqALyBwQ&usqp=CAU',
+		url: '#'
+	}
+];
