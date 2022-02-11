d6f5e01e3464d64439d14047efd7c30610da0d76
Ajboy Ian Abordo <ajboyian321@gmail.com>
2022-02-12 01:12:35 +0800

    Improve Portfolio UI

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,7 +1,7 @@
 import './Footer.css';
 
 import React from 'react';
-import { FaFacebookF, FaTwitter } from 'react-icons/fa';
+import { FaFacebookF, FaTwitter, FaGithub, FaEnvelope, FaLinkedin, FaInstagram, FaPhoneAlt } from 'react-icons/fa';
 
 function Footer() {
 	return (
@@ -13,11 +13,26 @@ function Footer() {
 
 				<div className="social-medias">
 					
+					<a href="tel:+639982889462">
+						<FaPhoneAlt className="phone-icon" />
+					</a>
 					<a href="#" target="_blank">
 						<FaFacebookF className="facebook-icon" />	
 					</a>
 					<a href="#" target="_blank">
 						<FaTwitter className="twitter-icon" />
+					</a>
+					<a href="mailto:ajboyian321@gmail.com" target="_blank">
+						<FaEnvelope className="gmail-icon" />
+					</a>
+					<a href="#" target="_blank">
+						<FaGithub className="github-icon" />
+					</a>
+					<a href="#" target="_blank">
+						<FaLinkedin className="linkedin-icon" />
+					</a>
+					<a href="#" target="_blank">
+						<FaInstagram className="instagram-icon" />
 					</a>
 
 				</div>
--- src/Components/Projects/my-projects.js
+++ src/Components/Projects/my-projects.js
@@ -11,13 +11,13 @@ export const myProjects = [
 		description: "This app is used by my partner in her work as an author/writer that tracks how many word counts she wrote in a specific day.",
 		stacks: ['HTML', 'CSS', 'SASS', 'Javascript', 'ReactJS', 'Laravel', 'MySQL'],
 		image: `${window.location.href}/images/word-count-tracker.png`,
-		url: '#'
+		url: ''
 	},
 	{
 		name: 'Project 3',
 		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
 		stacks: ['HTML', 'CSS', 'Javascript', 'ReactJS', 'MySQL'],
 		image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSN26hOftzocJIhBD4xrhROdm8fcchqALyBwQ&usqp=CAU',
-		url: '#'
+		url: ''
 	}
 ];
