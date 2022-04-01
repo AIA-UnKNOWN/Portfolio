6297ee1fd8d0b266d77dacd1d438508d67c76f0c
Ajboy Ian Abordo <ajboyian321@gmail.com>
2022-04-02 05:41:22 +0800

    Added a project

--- src/Components/Projects/my-projects.js
+++ src/Components/Projects/my-projects.js
@@ -7,17 +7,17 @@ export const myProjects = [
 		url: 'https://aia-unknown.github.io/cryptocurrency-converter/'
 	},
 	{
-		name: 'Word Count Tracker',
-		description: "This app is used by my partner in her work as an author/writer that tracks how many word counts she wrote in a specific day.",
-		stacks: ['HTML', 'CSS', 'SASS', 'Javascript', 'ReactJS', 'Laravel', 'MySQL'],
-		image: `${window.location.href}/images/word-count-tracker.png`,
-		url: ''
-	},
-	{
 		name: 'Bug Tracker',
-		description: 'Simpel and fast bug tracking system that can be used for daily work.',
+		description: 'Simple and fast bug tracking system that can be used for daily work.',
 		stacks: ['HTML', 'CSS', 'Tailwind CSS', 'Javascript', 'ReactJS', 'Redux', 'PHP', 'Laravel', 'MySQL'],
 		image: 'https://aia-unknown.github.io/Portfolio/images/bug-tracker.png',
 		url: 'https://github.com/AIA-UnKNOWN/bug-tracker'
-	}
+	},
+	{
+		name: 'Authors Text Editor',
+		description: "Easily document and track the progress of your stories, researches, documentations, and more.",
+		stacks: ['HTML', 'CSS', 'Tailwind CSS', 'Javascript', 'ReactJS', 'Redux', 'Jest', 'PHP', 'Laravel', 'MySQL'],
+		image: `https://aia-unknown.github.io/Portfolio/images/authors-text-editor.png`,
+		url: 'https://github.com/AIA-UnKNOWN/authors-text-editor'
+	},
 ];
