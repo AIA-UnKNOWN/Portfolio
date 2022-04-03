c5453dd7903e7a35bf9ce720e1bbf28f83b7ae61
Ajboy Ian Abordo <ajboyian321@gmail.com>
2022-04-03 22:40:48 +0800

    Added the actual link for the bug-tracker app project

--- src/Components/Projects/my-projects.js
+++ src/Components/Projects/my-projects.js
@@ -1,17 +1,10 @@
 export const myProjects = [
 	{
-		name: 'Cryptocurrency Converter',
-		description: 'This app converts a cryptocurrency value into another cryptocurrency.',
-		stacks: ['HTML', 'CSS', 'Javascript', 'Webpack'],
-		image: `${window.location.href}/images/cryptocurrency-converter.png`,
-		url: 'https://aia-unknown.github.io/cryptocurrency-converter/'
-	},
-	{
 		name: 'Bug Tracker',
 		description: 'Simple and fast bug tracking system that can be used for daily work.',
 		stacks: ['HTML', 'CSS', 'Tailwind CSS', 'Javascript', 'ReactJS', 'Redux', 'PHP', 'Laravel', 'MySQL'],
 		image: 'https://aia-unknown.github.io/Portfolio/images/bug-tracker.png',
-		url: 'https://github.com/AIA-UnKNOWN/bug-tracker'
+		url: 'http://bug-tracker.ajboyianabordo.online/'
 	},
 	{
 		name: 'Authors Text Editor',
@@ -19,5 +12,12 @@ export const myProjects = [
 		stacks: ['HTML', 'CSS', 'Tailwind CSS', 'Javascript', 'ReactJS', 'Redux', 'Jest', 'PHP', 'Laravel', 'MySQL'],
 		image: `https://aia-unknown.github.io/Portfolio/images/authors-text-editor.png`,
 		url: 'http://authors-text-editor.ajboyianabordo.online/'
+	},
+	{
+		name: 'Cryptocurrency Converter',
+		description: 'This app converts a cryptocurrency value into another cryptocurrency.',
+		stacks: ['HTML', 'CSS', 'Javascript', 'Webpack'],
+		image: `${window.location.href}/images/cryptocurrency-converter.png`,
+		url: 'https://aia-unknown.github.io/cryptocurrency-converter/'
 	},
 ];
